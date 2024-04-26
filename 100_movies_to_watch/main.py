d3b3bc1c23668da226712f8dbdae972426044655
Balint <balint.karoly13@gmail.com>
2024-04-26 14:10:35 +0200

    Complete 100 movies collector program with web scraping

--- 100_movies_to_watch/main.py
+++ 100_movies_to_watch/main.py
@@ -1,12 +1,22 @@
+# This program scraping a website to get top 100 movies, save it to a list
+# then reverse this list and write it to a txt file.
+
 import requests
 from bs4 import BeautifulSoup
 
+# archive link for the website
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
-
 response = requests.get(url=URL)
-movies_web_page = response.text
+website_html = response.text
 
-soup = BeautifulSoup(movies_web_page, "html.parser")
-print(soup.prettify())
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in all_movies]
+
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
