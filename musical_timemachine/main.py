89e211973eadb13cb66574e94ecf8f0e6528793f
BalintFarkas <balint.karoly13@gmail.com>
2024-05-05 11:48:50 +0200

    Complete musical timemachine project with webscraping and Spotify API

--- musical_timemachine/main.py
+++ musical_timemachine/main.py
@@ -13,17 +13,18 @@ SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME")
 
 
 # Get the data from the user - which date to webscraping
-# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
-#
-# date_url = f"https://www.billboard.com/charts/hot-100/{date}/"
-#
-# response = requests.get(url=date_url)
-# website_html = response.text
-#
-# soup = BeautifulSoup(website_html, "html.parser")
-# song_name_spans = soup.select("li ul li h3")
-# song_names = [song.getText().strip() for song in song_name_spans]
-# print(song_names)
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
+
+# Webscraping - save 100 songs name to a list
+date_url = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+response = requests.get(url=date_url)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+song_name_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_name_spans]
+
 
 # Auth with spotipy
 sp = spotipy.Spotify(
@@ -39,3 +40,28 @@ sp = spotipy.Spotify(
 )
 
 user_id = sp.current_user()["id"]
+
+
+# Get the URI for top 100 songs
+song_uris = []
+year = date.split("-")[0]
+
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    # print(result)
+    try:
+        song_uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(song_uri)
+    except IndexError:
+        print(f"Song: {song} not in Spotify - Skipped")
+
+# Create playlist to add music
+playlist_name = f"Top 100 songs in {date}"
+description = f"Python project for webscraping top 100 songs in {date} and creating a Spotify playlist."
+
+playlist_create = sp.user_playlist_create(user=user_id, name=playlist_name, public=False, description=description)
+print(playlist_create)
+
+playlist_id = playlist_create["id"]
+
+sp.playlist_add_items(playlist_id=playlist_id, items=song_uris)
