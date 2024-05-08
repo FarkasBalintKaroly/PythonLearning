7544bce83bc01d620c037bf2e84d9e120eb871b3
BalintFarkas <balint.karoly13@gmail.com>
2024-05-08 17:42:57 +0200

    Intrudoction to selenium web driver -- click and using search bar on wikipedia

--- day-48-selenium/interaction.py
+++ day-48-selenium/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -10,5 +11,15 @@ driver.get(url="https://en.wikipedia.org/wiki/Main_Page")
 article_number = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 print(article_number.text)
 
+# Click on anchor tag
+# article_number.click()
 
-driver.quit()
+# Find element by Link Text
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+# Using search bar
+search_bar = driver.find_element(By.NAME, value="search")
+search_bar.send_keys("Python", Keys.ENTER)
+
+# driver.quit()
