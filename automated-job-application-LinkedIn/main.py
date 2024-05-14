b2b891bbe4335d6b06dfe447034716ac922e8b47
Balint <balint.karoly13@gmail.com>
2024-05-14 18:57:28 +0200

    Complete job application app - selenium webdriver

--- automated-job-application-LinkedIn/main.py
+++ automated-job-application-LinkedIn/main.py
@@ -7,6 +7,7 @@ import time
 
 MY_EMAIL = os.environ.get("LINKEDIN_EMAIL")
 LINKEDIN_PASSWORD = os.environ.get("LINKEDIN_PASSWORD")
+PHONE_NUMBER = os.environ.get("PHONE_NUM")
 
 # Starting webdriver
 chrome_options = webdriver.ChromeOptions()
@@ -18,7 +19,7 @@ driver.get(url="https://www.linkedin.com/jobs/search/?currentJobId=3915033791&f_
 
 # Sign In to LinkedIn
 time.sleep(2)
-sign_in_button = driver.find_element(By.LINK_TEXT, value="Sign In")
+sign_in_button = driver.find_element(By.XPATH, value='/html/body/div[4]/a[1]')
 sign_in_button.click()
 
 email_bar = driver.find_element(By.XPATH, value='//*[@id="username"]')
@@ -29,3 +30,18 @@ password_bar.send_keys(LINKEDIN_PASSWORD)
 
 sign_in_button = driver.find_element(By.XPATH, value='//*[@id="organic-div"]/form/div[3]/button')
 sign_in_button.click()
+
+
+# Apply for a job
+time.sleep(5)
+apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+apply_button.click()
+
+time.sleep(5)
+phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+if phone.text == "":
+    phone.send_keys(PHONE_NUMBER)
+
+# Submit the application
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+submit_button.click()
