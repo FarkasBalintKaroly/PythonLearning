0a8a1575806b639ec2ff782f3445f281cbc6b182
BalintFarkas <balint.karoly13@gmail.com>
2024-05-27 21:26:24 +0200

    Upgraded blog with Bootstrap styling

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import requests
+
+posts_url = "https://api.npoint.io/674f5423f73deab1e9a7"
+
+all_posts = requests.get(url=posts_url).json()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def get_all_posts():
-    return render_template(template_name_or_list="index.html")
+    return render_template(template_name_or_list="index.html", posts=all_posts)
 
 
 @app.route("/about")
@@ -16,6 +21,15 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template(template_name_or_list="contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for post in all_posts:
+        if post["id"] == index:
+            requested_post = post
+    return render_template(template_name_or_list="post.html", post=requested_post)
 
 
 if __name__ == "__main__":
