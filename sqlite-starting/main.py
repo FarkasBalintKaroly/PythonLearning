ca8e6c6e467935627be52e316a8d473ebae9ff00
BalintFarkas <balint.karoly13@gmail.com>
2024-06-06 20:04:59 +0200

    Intruduction to SQLite and SQLAlchemy

--- sqlite-starting/main.py
+++ sqlite-starting/main.py
@@ -1,7 +1,39 @@
-import sqlite3
+# Introducing with SQLite3
 
-db = sqlite3.connect("books-collection.db")
+# import sqlite3
 
-cursor = db.cursor()
+# db = sqlite3.connect("books-collection.db")
 
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor = db.cursor()
+
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) "
+#                "NOT NULL, rating FLOAT NOT NULL)")
+
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+
+
+# Do the same with SQLAlchemy
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    author: Mapped[str]
+    rating: Mapped[float]
+
+
+db = SQLAlchemy(model_class=Base)
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+db.init_app(app)
