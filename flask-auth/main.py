6a8d5d2cb9cb9014424256af09186ba732fc12ac
BalintFarkas <balint.karoly13@gmail.com>
2024-06-22 15:27:02 +0200

    Fix authentication for user

--- flask-auth/main.py
+++ flask-auth/main.py
@@ -1,3 +1,4 @@
+import werkzeug.security
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -19,18 +20,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Create login manager user loader
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
 # CREATE TABLE IN DB
-
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
 
-
 with app.app_context():
     db.create_all()
+
 
 
 @app.route('/')
@@ -40,31 +48,65 @@ def home():
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
+    error = None
     if request.method == "POST":
-        user = User(
-            email=request.form["email"],
-            password=request.form["password"],
-            name=request.form["name"],
-        )
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('secrets'))
-    return render_template("register.html")
+        email = request.form["email"]
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if not user:
+            unsecure_password = request.form["password"]
+            secure_password = generate_password_hash(
+                password=unsecure_password,
+                method='pbkdf2:sha256',
+                salt_length=8
+            )
+
+            new_user = User(
+                email=request.form["email"],
+                password=secure_password,
+                name=request.form["name"]
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+            login_user(new_user)
+
+            return redirect(url_for('secrets'))
+        if user:
+            error = "You've already signed up with that e-mail. Try to log in instead."
+
+    return render_template("register.html", error=error)
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if user:
+            if check_password_hash(pwhash=user.password, password=password):
+                login_user(user)
+                return redirect(url_for("secrets"))
+            else:
+                error = "Password incorrect. Please try again."
+        elif not user:
+            error = "That e-mail does not exists. Please try again."
+
+    return render_template("login.html", error=error)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
