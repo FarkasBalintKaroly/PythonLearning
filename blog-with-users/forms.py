ea558524f3a148f9062d4ccf42956e02153d60f6
BalintFarkas <balint.karoly13@gmail.com>
2024-06-24 15:35:28 +0200

    Fix user register and log in

--- blog-with-users/forms.py
+++ blog-with-users/forms.py
@@ -13,14 +13,18 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a RegisterForm to register new users
+# WTForm for registering to the blog
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign Me Up!")
 
-# TODO: Create a LoginForm to login existing users
 
+# WTForm for log in to the blog
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
 
 # TODO: Create a CommentForm so users can leave comments below posts
